fc67843607397290f02836a2d42e477acc5eefd4
eemans21 <74782082+eemans21@users.noreply.github.com>
2020-11-20 10:25:06 -0500

    Update and rename Euler_Method to Euler_Method.m

--- Euler_Method.m
+++ Euler_Method.m
@@ -1,7 +1,10 @@
 function [t,x1,x2] = euler_1(f,g,t0,tN,x0_1,x0_2,h)
+
 x1(1)=x0_1;
 x2(1)=x0_2;
+
 t = t0:h:tN;
+
     for i=1:((tN-t0)/h)
         x1(i+1)=x1(i)+h*f(t(i),x1(i));
         x2(i+1)=x2(i)+h*g(t(i),x2(i));
